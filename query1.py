a70cf455edd44396968e8e9bbe8e6834c5d340fb
Rafik Hachana <rafikhachana@gmail.com>
2021-04-22 19:08:55 +0300

    Update query1.py

--- query1.py
+++ query1.py
@@ -1,25 +1,23 @@
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from datetime import datetime
-import timeit
 
 cred = credentials.Certificate('certificate.json')
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL' : 'https://assignment-2-e7286-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https://assignment-2-e7286-default-rtdb.firebaseio.com/'
 })
 
 
 def query_1_local():
-
-    # Making a joint table
+    # Acquiring all tables
     payments = list(db.reference('payment').order_by_child('rental_id').get())
     rentals = list(db.reference('rental').order_by_child('rental_id').get())
 
+    # The tables are sorted, so we can use 2 pointers to match the rental_id
     p_i = 0
-    res = []
+    joint_table = []
     for i in rentals:
         while payments[p_i]['rental_id'] < i['rental_id']:
             p_i += 1
@@ -27,78 +25,98 @@ def query_1_local():
             tmp = payments[p_i]
             for j in i:
                 tmp[j] = i[j]
-            res.append(tmp)
+            joint_table.append(tmp)
 
     # Making the aggregation
     payments_by_amount = list(db.reference('payment').order_by_child('amount').get())
-    for i in res:
-        print("Doing b search")
-        # Binary search the answer
+    for i in joint_table:
 
+        # Binary search the answer
         low = 0
         high = len(payments_by_amount)
-        while(low<high):
-            #print(str(low) + ' -> ' + str(high))
-            if low<0:
+        while low < high:
+            if low < 0:
                 low = -1
                 break
-            mid = (low+high)//2
+            mid = (low + high) // 2
             if payments_by_amount[mid]['amount'] < i['amount']:
-                low = mid+1
+                low = mid + 1
             else:
                 high = mid
         i['count_smaller_pay'] = low
 
-    return res
-
-# This can be used to listen to value changes in the database
-'''def cb(something):
-    print("A change was made to the database.")
-    print(something.event_type)
-    print(something.path)
-    #print(something.data)'''
+    return joint_table
 
 
 
 
 
-
-def query1():
+# The next function is just an example of a slow way to perform join
+def query_1_join_operation_slow():
     payments = list(db.reference('payment').get())
     counter = 3
     for i in payments:
         print(i)
-        counter-=1
+        counter -= 1
         if not counter:
             break
     res = []
 
+    #The slow part : getting the rental information directly from the database
     for i in payments:
         rental = list(dict(db.reference('rental').order_by_child('rental_id').equal_to(i['rental_id']).get()).values())
         tmp = i
-        if len(rental)>1:
-            print('Found more then one corresponding rental')
-            break
-
-        if len(rental)==0:
-            print('We have a problem, rental is empty')
+        # In case there is no corresponding rental
+        if len(rental) == 0:
             continue
 
-        #print(rental[0])
+        # We are assuming that there is only one rental for each payment
         for j in rental[0]:
             tmp[j] = rental[0][j]
-        #print(tmp)
         res.append(tmp)
-
     return res
 
 
-before = datetime.now()
-result = query_1_local()
-after = datetime.now()
-delta = after - before
-print(result)
-print("Total time to complete the query : " + str(delta.total_seconds()))
+# This is the fastest version of the function, using denormalization
+def query_1_with_extra_table():
+    table = db.reference('payment_rental_smaller_pay').get()
+    if table is None:
+        print("The table doesn't exist. Going to compute the table content...")
+        new_table = query_1_local()
+        db.reference('payment_rental_smaller_pay').set(new_table)
+        return new_table
+    return table
 
-#print(timeit.timeit(query1))
-db.reference().listen(cb)
+# print("Executing query 1")
+# before = datetime.now()
+# result = query_1_with_extra_table()
+# after = datetime.now()
+# delta = after - before
+# print(result)
+# print("Total time to complete the query : " + str(delta.total_seconds()))
+
+
+# This is an example on how to add a listener of the database
+# This can be used to listen to value changes in the database
+# This function updates the joint table of rentals_and_payments
+def cb(something):
+    # print("A change was made to the database.")
+    # print(something.event_type)
+    # print(something.path)
+    # print(something.data)
+    if something.path == '/':
+        return
+    if something.event_type == 'put':
+        print("An entry was added to the database.")
+        rental = db.reference('rental').order_by_child('rental_id').equal_to(something.data['rental_id']).get()
+        if rental is None:
+            return
+        tmp = something.data
+        for x in rental:
+            tmp[x] = rental[x]
+        db.reference('payment_rental_smaller_pay').child(something.path[1:]).set(something.data)
+            
+
+
+#We are going to set the listener to listen to new payments
+db.reference('payment').listen(cb)
